c218195b58f7ca7af66779977924db49e4e13f03
Samoilenko <samoylenko.alexey95@gmail.com>
2019-09-04 11:49:38 +0600

    восьмой commit

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -4,8 +4,8 @@ window.addEventListener('DOMContentLoaded', function() {
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
         
-    function hideTabContent(a) {
-        for(let i = a; i < tabContent.length;  i++){
+    function hideTabContent(a){
+        for(let i = a; i < tabContent.length; i++){
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     hideTabContent(1);
 
-    function showTabContent(b) {
+    function showTabContent(b){
         if(tabContent[b].classList.contains('hide')){
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
@@ -32,4 +32,42 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Timer
+
+    let deadline = "2019-09-05";
+
+    function getTimeRemaining(endtime) {
+        let option = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((option/1000) % 60),
+            minutes = Math.floor((option/1000/60) % 60),
+            hours = Math.floor((option/(1000*60*60)));
+
+            return {
+                'total': option,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+    
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let options = getTimeRemaining(endtime);
+            hours.textContent = options.hours;
+            minutes.textContent = options.minutes;
+            seconds.textContent = options.seconds;
+            if(options.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('timer',deadline);
 });
